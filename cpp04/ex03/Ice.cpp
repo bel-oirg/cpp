5bfef8fa68d7a87c9228a15cf23cb81f54c781d0
Badr Eddine El Oirghi <bel-oirg@e3r3p17.1337.ma>
2024-07-13 17:35:56 +0100

    CPP4 :)

--- cpp04/ex03/Ice.cpp
+++ cpp04/ex03/Ice.cpp
@@ -6,7 +6,7 @@
 /*   By: bel-oirg <bel-oirg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 10:52:46 by bel-oirg          #+#    #+#             */
-/*   Updated: 2024/07/13 12:17:45 by bel-oirg         ###   ########.fr       */
+/*   Updated: 2024/07/13 12:50:20 by bel-oirg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void Ice::use(ICharacter& target)
 {
-    cout << "* shoots an ice bolt at <<  " << target.getName() << " *" << endl;
+    cout << "* shoots an ice bolt at " << target.getName() << " *" << endl;
 }
 
 Ice::Ice() : AMateria("ice")
